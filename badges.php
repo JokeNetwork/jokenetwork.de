48abbcef504a31a238cd26405a30573d5c2f74c5
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-08-30 13:26:30 +0200

    Added badges + small fixes in css

---
+++ badges.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Badges | JokeNetwork - Open Web</title>
+    <meta charset="UTF-8">
+
+    <meta name="description" content="We're a young team of webdesigners and web-developers and we're offering (ads-)free tools and apps as well as open source projects.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
+    <link rel="mask-icon" href="/assets/img/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/assets/img/favicon.ico">
+
+    <link href="assets/css/style.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+      <header>
+        <div class="container">
+          <div class="logo">
+            <a href="//jokenetwork.de"><img src="assets/img/plain.svg" alt="Logo"></a>
+          </div>
+          <nav>
+            <a href="//github.com/jokenetwork">GitHub</a>
+          </nav>
+        </div>
+      </header>
+
+      <div class="content">
+       <section class="message">
+        <h1>GitHub Badges</h1>
+          <p>We love the idea of open-sourced projects! <br>But sometimes it is hard to see if a project is still actively maintained or not.<br>
+          Because of this, we created the fowllowing Badges for README-overviews on GitHub.</p>
+          
+            <table>
+              <tbody>
+                  <tr>
+                  <td><img src="/assets/img/gitstatus/communitybased.svg" alt="Status: Active"></td>
+                  <td><span class="bold">Community based:</span><br>This badge indicates that the project is still actively maintained by the community.</td>
+                </tr>
+                <tr>
+                  <td><img src="/assets/img/gitstatus/active.svg" alt="Status: Active"></td>
+                  <td><span class="bold">Active:</span><br>This badge indicates that the project is actively maintained.</td>
+                </tr>
+                <tr>
+                  <td><img src="/assets/img/gitstatus/inactive.svg" alt="Status: Inactive"></td>
+                  <td><span class="bold">Inactive:</span><br>This badge indicates that the project is not actively maintained, but that might change in the future.</td>
+                </tr>
+                <tr>
+                  <td><img src="/assets/img/gitstatus/discontinued.svg" alt="Status: Discontinued"></td>
+                  <td><span class="bold">Discontinued:</span><br>This badge indicates that the project is not actively maintained and will not be maintained in the future.</td>
+                </tr>
+                <tr>
+                  <td><img src="/assets/img/gitstatus/unknown.svg" alt="Status: Unknown"></td>
+                  <td><span class="bold">Unknown:</span><br>This badge indicates that there is not information about the project being maintained or not.</td>
+                </tr>
+              </tbody>
+            </table>
+        </section>
+      </div>
+    </div>
+      <?php include ('includes/footer.php'); ?>
+  </body>
+</html>
