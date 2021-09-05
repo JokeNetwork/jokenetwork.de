8b2ae56cdbb82b5b99566a87f5c0e3be97c11f26
Philip <4144601+philipbrembeck@users.noreply.github.com>
2021-09-05 15:15:53 +0200

    Updated to absolut path

--- 404.php
+++ 404.php
@@ -7,20 +7,20 @@
     <meta name="description" content="We're a young team of webdesigners and web-developers and we're offering (ads-)free tools and apps as well as open source projects.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
-    <link rel="mask-icon" href="/assets/img/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" href="/assets/img/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://jokenetwork.de/assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://jokenetwork.de/assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://jokenetwork.de/assets/img/favicon-16x16.png">
+    <link rel="mask-icon" href="https://jokenetwork.de/assets/img/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="https://jokenetwork.de/assets/img/favicon.ico">
 
-    <link href="assets/css/style.min.css" rel="stylesheet">
+    <link href="https://jokenetwork.de/assets/css/style.min.css" rel="stylesheet">
   </head>
 
   <body>
     <header>
       <div class="container">
         <div class="logo">
-          <a href="//jokenetwork.de"><img src="assets/img/plain.svg" alt="Logo"></a>
+          <a href="//jokenetwork.de"><img src="https://jokenetwork.de/assets/img/plain.svg" alt="Logo"></a>
         </div>
         <nav>
           <a href="//github.com/jokenetwork">GitHub</a>
@@ -35,6 +35,7 @@
       </section> 
     </div>
     </main>
-    <?php include ('includes/footer.php'); ?>
+    <?php 
+    include ('/var/www/virtual/jake/jokenetwork.de/includes/footer.php'); ?>
   </body>
 </html>
